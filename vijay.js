9e75e2aa76af52d6c67e465fb3971e0c8a104b7a
VIjay Singh <146606880+Vijaysingh966@users.noreply.github.com>
2025-01-24 20:46:23 +0530

    Add files via upload

--- vijay.js
+++ vijay.js
@@ -1,9 +1,6 @@
 const form = document.getElementById("form");   
 const submit = document.getElementById("submit");  
 const maincontainer = document.getElementById("main-container");
- 
-
-
 
 submit.addEventListener("click", (event) => {
     event.preventDefault(); 
@@ -16,7 +13,7 @@ submit.addEventListener("click", (event) => {
     if (Thumbnai && Title && Channel && views && Month && Duration) {
         createCard(Thumbnai,Title, Channel, views,Month,Duration);
         alert("Your card is now created")
-        // document.getElementById("cardForm").reset();  // Reset form after submission
+         
       } else {
         alert("Please fill in all fields!");
       } 
@@ -24,15 +21,15 @@ submit.addEventListener("click", (event) => {
 })
 function formatNumber(number) {
   if (number >= 1e9) {
-    return (number / 1e9).toFixed(1) + 'B';  // Billion
+    return (number / 1e9).toFixed(1) + 'B';  
   }
   if (number >= 1e6) {
-    return (number / 1e6).toFixed(1) + 'M';  // Million
+    return (number / 1e6).toFixed(1) + 'M';  
   }
   if (number >= 1e3) {
-    return (number / 1e3).toFixed(1) + 'K';  // Thousand
+    return (number / 1e3).toFixed(1) + 'K';  
   }
-  return number;  // For numbers less than 1000
+  return number;  
 }
 
 function createCard(Thumbnai,Title,views,Channel,Month,Duration){
@@ -40,7 +37,8 @@ function createCard(Thumbnai,Title,views,Channel,Month,Duration){
     div.classList.add ("div");
     div.style.width = "100%";
     div.style.height = "50%"; 
-    div.style.display="flex"
+    div.style.display="flex";
+    div.style.position = "relative"
 
     const imgUrl = document.createElement("img");
     imgUrl.src = Thumbnai;
@@ -62,13 +60,23 @@ function createCard(Thumbnai,Title,views,Channel,Month,Duration){
      const cardDuration = document.createElement("p");
      cardDuration.textContent = Duration;
 
+     cardDuration.style.position = "absolute";
+     cardDuration.style.top = "97px";
+     cardDuration.style.left = "181px";
+     cardDuration.style.backgroundColor = "#000000b8";
+     cardDuration.style.color = "white";
+     cardDuration.style.borderRadius ="5px";
+     cardDuration.style.padding = "4px";
+     cardDuration.style.fontSize= "12px";
+     cardDuration.style.fontWeight= "bold";
+
      div.appendChild(imgUrl)
     div.appendChild(cardTitle);
     div.appendChild(cardviews);
     div.appendChild(cardCHannel);
     div.appendChild(cardMonth);
     div.appendChild(cardDuration);
-  maincontainer.appendChild(div);
+    +maincontainer.appendChild(div);
 }
 
 
